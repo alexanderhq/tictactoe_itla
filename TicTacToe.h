4f0de84a4b0669f7b8d3ad7f4ec306cb52d522af
alexanderhq <alexanderhq13@gmail.com>
2016-06-06 23:17:01 -0400

    you add a few methods and variables
    you add a few methods and variables to my comfort with the proviso that the teacher is very well done

---
+++ TicTacToe.h
@@ -0,0 +1,25 @@
+/*
+ * TicTacToe.h
+ *
+ *  Created on: May 22, 2016
+ *      Author: raydelto
+ */
+
+#ifndef TICTACTOE_H_
+#define TICTACTOE_H_
+#include <iostream>
+
+void init();
+bool validate();
+void clearScreen();
+void showBoard();
+bool isValidInput(std::istream&);
+bool isAvailable(int , int );
+bool gameOver(); //implement this method
+bool wineer();
+
+
+
+
+
+#endif /* TICTACTOE_H_ */
